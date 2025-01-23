61da928a712f96fbc919545b986ca2251c37a436
saraitmanar <saraitmanar@gmail.com>
2025-01-22 21:04:30 -0500

    the api key :)

---
+++ src/main/java/com/weatherapp/api/ApiKey.java
@@ -0,0 +1,10 @@
+package com.weatherapp.api;
+
+
+public class ApiKey {
+
+    private static final String APIkey = "23dcdbb187fa219b1ae591bbf5b1379d";
+    private static final String BaseURL = "https://api.openweathermap.org/data/2.5/weather";
+
+
+}
